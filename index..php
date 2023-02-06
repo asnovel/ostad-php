96ff505a677c69df23676291a42690716577f904
asnovel <sayemsayem1414@gmail.com>
2023-02-06 16:51:37 +0600

    updated

--- index..php
+++ index..php
@@ -1,3 +1,7 @@
-$fname="isaac";
-$lname="newton";
-printf("his name is %a %a,"$fname,$lname")
+$tuitionFee=16000;
+
+
+$commission = $tuitionFee < 7000 ? 'Invalid' : ($tuitionFee < 10000 ? 0.15 * $tuitionFee . " is the commission of tuition fee"  : ($tuitionFee < 20000 ? 0.2 * $tuitionFee . " is the commission of tuition fee" : 0.25 * $tuitionFee));  
+echo $commission;
+
+
