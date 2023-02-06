efd82e6a0c203e15d37df188893b477acc0dffa6
asnovel <sayemsayem1414@gmail.com>
2023-02-06 16:56:01 +0600

    added

--- index..php
+++ index..php
@@ -1,6 +1,4 @@
 $tuitionFee=16000;
-
-
 $commission = $tuitionFee < 7000 ? 'Invalid' : ($tuitionFee < 10000 ? 0.15 * $tuitionFee . " is the commission of tuition fee"  : ($tuitionFee < 20000 ? 0.2 * $tuitionFee . " is the commission of tuition fee" : 0.25 * $tuitionFee));  
 echo $commission;
 
